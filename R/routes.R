440a9e1c280dff4ff7286b42f7e3d82c8912da56
Jean-Mathieu Potvin <jm@potvin.xyz>
2021-02-05 11:58:42 -0500

    Add static routes to use in pkg. Temporary work.

--- R/routes.R
+++ R/routes.R
@@ -3,3 +3,15 @@
 # TODO(JMP): create simple routing systems for API calls. Routes should be
 # constant and encapsulated into really simple (internal?) R6 classes. They
 # will be called by future higher-level mechanisms.
+
+# In what follows, Q means river discharge and N water level.
+# In what follows, STATION CODE is a character string, usually 6 digits.
+
+# Route for stations' codes.
+"https://www.cehq.gouv.qc.ca/hydrometrie/historique_donnees/default.asp"
+
+# Route for station's data.
+"https://www.cehq.gouv.qc.ca/depot/historique_donnees/fichier/[STATION CODE]_[Q|N].txt"
+
+# Route for station's metadata
+"https://www.cehq.gouv.qc.ca/hydrometrie/historique_donnees/fiche_stations.asp?NoStation=[STATION CODE]"
