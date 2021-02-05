777c5f6f1e71d416ea0b97dda5c75e7bd3c562a3
Jean-Mathieu Potvin <jm@potvin.xyz>
2021-02-05 10:46:50 -0500

    Adding vision for script. This will guide us.

--- R/routes.R
+++ R/routes.R
@@ -1,3 +1,5 @@
 # Routes to data sources.
 
-# TBD.
+# TODO(JMP): create simple routing systems for API calls. Routes should be
+# constant and encapsulated into really simple (internal?) R6 classes. They
+# will be called by future higher-level mechanisms.
